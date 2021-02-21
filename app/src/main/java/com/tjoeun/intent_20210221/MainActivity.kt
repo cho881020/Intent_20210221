53a174edd8e228829c763625925d521673a177a1
조경진 <cho881020@gmail.com>
2021-02-21 15:45:50 +0900

    문자 화면에 기본 문구 추가

--- app/src/main/java/com/tjoeun/intent_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/intent_20210221/MainActivity.kt
@@ -84,6 +84,10 @@ class MainActivity : AppCompatActivity() {
 
 //            Intent -> Action + URI 전달.
             val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+
+//            문자 내용 기본값 입력. => 문자화면에 데이터 첨부.
+            myIntent.putExtra("sms_body", "원하는 기본 메세지")
+
             startActivity(myIntent)
 
         }
